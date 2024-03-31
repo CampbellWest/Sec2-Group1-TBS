b080da54c4012767d716c817a2fb31957082d708
Campbell West <campbellwest1@gmail.com>
2024-03-31 15:38:15 -0400

    Added files for winConditions and dealerHits. win conditions is good to add in but dealer function isnt working yet

---
+++ ProjectII/DealerHits.c
@@ -0,0 +1,16 @@
+#include "Hand.h"
+
+void dealerHitsOrStands(DECK* deck, HAND* dealer, HAND* player) {
+
+    do {
+        if (dealer->totalCardValue >= 17) {
+            return;
+        }
+
+        //Draw(dealer, deck, )
+        //print the new cards for dealer 
+        //DRAW A CARD
+
+
+    } while (dealer->totalCardValue < 17 || dealer->totalCardValue < player->totalCardValue);
+}
