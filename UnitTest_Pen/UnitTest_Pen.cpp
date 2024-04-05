8b15baf99e61e4c31021cd9a65fe82132a62696e
Zongping Cui <Zcui1597@conestogac.on.ca>
2024-04-05 18:08:56 -0400

    blank version

---
+++ UnitTest_Pen/UnitTest_Pen.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestPen
+{
+	TEST_CLASS(UnitTestPen)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
